2e06945eb35f99e8b4837d0d5db8647e8005b876
suchitra-das <sdas90933@gmail.com>
2026-03-03 14:47:27 +0530

    react todo

---
+++ contextapi-todo/src/context/todoContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const todoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "todo msg",
+      completed: false,
+    },
+  ],
+
+  //  addTodo : (todo) => { },
+  //   updatedTodo : (id, todo) =>{},
+  //   deleteTodo : (id) =>{},
+  //   toggleComplete : (id) =>{}
+});
+
+export const useTodo = () => {
+  return useContext(todoContext);
+};
+
+export const TodoProvider = todoContext.Provider;
